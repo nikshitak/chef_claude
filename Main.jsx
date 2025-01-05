36526c145f68c4cf9d71293c3fdfa5ccfaa0e24c
Nikshita Kurva <nikkikurva@gmail.com>
2025-01-04 21:59:27 -0600

    initialization

---
+++ Main.jsx
@@ -0,0 +1,10 @@
+export default function Main() {
+    return (
+        <main>
+            <form>
+                <input />
+                <button></button>
+            </form>
+        </main>
+    )
+}n
